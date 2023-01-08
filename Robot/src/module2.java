bd71b11fba6c5b449427b82d857ce15ca53e4b89
Alexandr Romanovskiy <ma4e94@gmail.com>
2023-01-08 18:25:31 +0300

    Задание на степике про робота Дистанционное подключение

---
+++ Robot/src/module2.java
@@ -0,0 +1,127 @@
+public class module2 {
+    public static void main(String[] args) {
+        Robot robot = new Robot(1, 2, Direction.DOWN);
+        moveRobot(robot, 1, 5);
+        System.out.println(robot.getX() + " " + robot.getY());
+    }
+
+    public enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    public static class Robot {
+        int x;
+        int y;
+        Direction dir;
+
+        public Robot(int x, int y, Direction dir) {
+            this.x = x;
+            this.y = y;
+            this.dir = dir;
+        }
+
+        public Direction getDirection() {
+            return dir;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void turnLeft() {
+            if (dir == Direction.UP) {
+                dir = Direction.LEFT;
+            } else if (dir == Direction.DOWN) {
+                dir = Direction.RIGHT;
+            } else if (dir == Direction.LEFT) {
+                dir = Direction.DOWN;
+            } else if (dir == Direction.RIGHT) {
+                dir = Direction.UP;
+            }
+        }
+
+        public void turnRight() {
+            if (dir == Direction.UP) {
+                dir = Direction.RIGHT;
+            } else if (dir == Direction.DOWN) {
+                dir = Direction.LEFT;
+            } else if (dir == Direction.LEFT) {
+                dir = Direction.UP;
+            } else if (dir == Direction.RIGHT) {
+                dir = Direction.DOWN;
+            }
+        }
+
+        public void stepForward() {
+            if (dir == Direction.UP) {
+                y++;
+            }
+            if (dir == Direction.DOWN) {
+                y--;
+            }
+            if (dir == Direction.LEFT) {
+                x--;
+            }
+            if (dir == Direction.RIGHT) {
+                x++;
+            }
+        }
+    }
+
+    public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
+        for (int i = 0; i < 3; i++) {
+            try (RobotConnection connection = robotConnectionManager.getConnection()){
+                connection.moveRobotTo(toX, toY);
+                i = 2;
+            } catch (RobotConnectionException e) {
+                if (i == 2) {
+                    throw new RobotConnectionException("error");
+                }
+            }
+        }
+    }
+
+    public static void moveRobot(Robot robot, int toX, int toY) {
+        int iX = robot.getX();
+        int iY = robot.getY();
+        if (toX > iX) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+        }
+        if (toX < iX) {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+        }
+        if (toY > iY) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnLeft();
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        }
+        if (toY < iY) {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        }
+    }
+}
